54b32a5fae88f913045277ceb0bafa219ed846bd
AndrevvRoman <andrey_romanof@mail.ru>
2021-10-07 15:18:21 +0700

    Переделал на процедурные подход

--- include/ReversePolishConverter.hpp
+++ include/ReversePolishConverter.hpp
@@ -1,8 +1,97 @@
 #pragma once
-#include <string>
+#include "Utils.hpp"
 
-class ReversePolishConverter
+#include <string>
+#include <stack>
+#include <algorithm>
+
+int priority(const std::string literal)
 {
-    public:
-    std::string convert(const std::string & input);
-};
+    if (literal == "+" || literal == "-")
+    {
+        return 1;
+    }
+    else if (literal == "*" || literal == "/")
+    {
+        return 2;
+    }
+    else if (literal == "(")
+    {
+        return 3;
+    }
+    else if (literal == ")")
+    {
+        return 0;
+    }
+    return -1;
+}
+
+std::string convertToReversePolish(const std::string &_input)
+{
+    std::string input = _input;
+    replaceAll(input, "+", " + ");
+    replaceAll(input, "-", " - ");
+    replaceAll(input, "*", " * ");
+    replaceAll(input, "/", " / ");
+    replaceAll(input, "(", " ( ");
+    replaceAll(input, ")", " ) ");
+
+    auto literals = split(input, ' ');
+    std::stack<std::string> operators;
+    std::string result = "";
+    bool scope = false;
+    for (const auto &literal : literals)
+    {
+        if (literal == "")
+        {
+            continue;
+        }
+        if (is_number(literal))
+        {
+            result += literal;
+        }
+        else
+        {
+
+            if (!operators.empty() && (priority(operators.top()) >= priority(literal)))
+            {
+                if (literal == ")")
+                {
+                    while (operators.top() != "(")
+                    {
+                        result += operators.top();
+                        operators.pop();
+                    }
+                    operators.pop(); // pops open scope
+                }
+                else
+                {
+                    while (!operators.empty())
+                    {
+                        if (operators.top() == "(")
+                        {
+                            break;
+                        }
+                        result += operators.top();
+                        operators.pop();
+                    }
+                }
+            }
+            if (literal != ")")
+            {
+                operators.push(literal);
+            }
+        }
+    }
+
+    if (!operators.empty())
+    {
+        while (!operators.empty())
+        {
+            result += operators.top();
+            operators.pop();
+        }
+    }
+
+    return result;
+}
--- main.cpp
+++ main.cpp
@@ -4,13 +4,12 @@
 #include "Calculator.hpp"
 
 int main(int argc, char** argv) {
-    Calculator c;
     std::string input;
     for(int i = 1; i < argc; i++)
     {
         input += argv[i];
     }
     std::cout << "inputed = " << input << std::endl;
-    std::cout << c.count(input) << std::endl;
+    std::cout << count(input) << std::endl;
     return 0;
 }
