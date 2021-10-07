b9e4a953a7c8131a2a140abe37341a259cce275d
AndrevvRoman <andrey_romanof@mail.ru>
2021-10-07 15:04:13 +0700

    Стилистические правки

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include "Calculator.hpp"
 
 int main(int argc, char** argv) {
-    // ReversePolishConverter c;
     Calculator c;
     std::string input;
     for(int i = 1; i < argc; i++)
@@ -12,7 +11,6 @@ int main(int argc, char** argv) {
         input += argv[i];
     }
     std::cout << "inputed = " << input << std::endl;
-    std::cout << c.count("2*(2+2)+2") << std::endl;
-    // std::cout << c.convert(input) << std::endl;
+    std::cout << c.count(input) << std::endl;
     return 0;
 }
